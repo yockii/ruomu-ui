41a02ab2fde613e4e275a1fe8f86fea470b2c28d
yockii <xuyuqi@gmail.com>
2024-09-26 10:38:33 +0800

    feat: page add field: desc/fileName

--- model/Page.go
+++ model/Page.go
@@ -5,12 +5,14 @@ import (
 )
 
 type Page struct {
-	ID        uint64 `json:"id,omitempty,string" gorm:"primaryKey"`
-	ProjectID uint64 `json:"projectId,omitempty,string" gorm:"comment:'项目ID'"`
-	Name      string `json:"name,omitempty" gorm:"size:50;comment:'页面名称'"`
-	ParentID  uint64 `json:"parentId,omitempty,string" gorm:"comment:'父页面ID'"`
-	Route     string `json:"route,omitempty" gorm:"size:50;comment:'页面路由'"`
-	Schema    string `json:"schema,omitempty" gorm:"text;comment:'页面配置'"`
+	ID          uint64 `json:"id,omitempty,string" gorm:"primaryKey"`
+	ProjectID   uint64 `json:"projectId,omitempty,string" gorm:"comment:'项目ID'"`
+	Name        string `json:"name,omitempty" gorm:"size:50;comment:'页面名称'"`
+	Description string `json:"description,omitempty" gorm:"size:255;comment:'页面描述'"`
+	ParentID    uint64 `json:"parentId,omitempty,string" gorm:"comment:'父页面ID'"`
+	Route       string `json:"route,omitempty" gorm:"size:50;comment:'页面路由'"`
+	FileName    string `json:"fileName,omitempty" gorm:"size:50;comment:'文件名'"`
+	Schema      string `json:"schema,omitempty" gorm:"text;comment:'页面配置'"`
 
 	CreateTime int64 `json:"createTime" gorm:"autoCreateTime"`
 }
@@ -24,8 +26,10 @@ func (p *Page) UnmarshalJSON(b []byte) error {
 	p.ID = j.Get("id").Uint()
 	p.ProjectID = j.Get("projectId").Uint()
 	p.Name = j.Get("name").String()
+	p.Description = j.Get("description").String()
 	p.ParentID = j.Get("parentId").Uint()
 	p.Route = j.Get("route").String()
+	p.FileName = j.Get("fileName").String()
 	p.Schema = j.Get("schema").String()
 
 	return nil
